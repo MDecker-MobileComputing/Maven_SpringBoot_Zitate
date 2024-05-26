f55ae98248ee6c0443351b66e40db582f8270c54
MDecker-MobileComputer <mide_42b@yahoo.de>
2024-05-26 10:18:57 +0200

    Projekt erstellt mit Spring Initializr https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.3.0&packaging=jar&jvmVersion=17&groupId=de.eldecker.dhbw.spring&artifactId=zitate&name=Zitate-Sammlung&description=Zitate-Sammlung%3A%20Demo%20f%C3%BCr%20Textsuche%20mit%20Lucene&packageName=de.eldecker.dhbw.spring.zitate&dependencies=data-jpa,h2,thymeleaf,web

---
+++ src/main/java/de/eldecker/dhbw/spring/zitate/ZitateSammlungApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.zitate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZitateSammlungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZitateSammlungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/zitate/ZitateSammlungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.zitate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZitateSammlungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
