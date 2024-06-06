190fb539c55952d331c073224fdd2b42787c7fc9
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-06 15:14:58 +0200

    ZitateRepo: Kommentar fuer Anpassung native Query fuer Postgres

--- src/main/java/de/eldecker/dhbw/spring/zitate/db/ZitateRepo.java
+++ src/main/java/de/eldecker/dhbw/spring/zitate/db/ZitateRepo.java
@@ -14,7 +14,12 @@ public interface ZitateRepo extends JpaRepository<ZitatEntity, Long> {
     /**
      * Native Query (!) für das Holen eines zufälligen Zitats, kein JPQL!
      * Funktioniert evtl. nicht, wenn andere Datenbank als "H2" verwendet wird.
-     *
+     * <br><br>
+     * 
+     * Das SQL-Statement funktioniert unter H2 (und anscheinend auch MySQL),
+     * aber nicht unter PostgreSQL: für PostgreSQL muss der Name der Funktion
+     * "RAND" durch "RANDOM" ersetzt werden.
+     * 
      * @return Optional mit zufällig ausgewähltem Zitat
      */
     @Query( nativeQuery = true, value = "SELECT * FROM zitate ORDER BY RAND() LIMIT 1" )
